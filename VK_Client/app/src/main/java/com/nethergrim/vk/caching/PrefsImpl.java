c892d0874a7687eb6560e4cb7c00cbf60d35285e
Andrew.Drobyazko <c2q9450@gmail.com>
2015-03-20 19:09:28 +0200

    implemented palette

--- VK_Client/app/src/main/java/com/nethergrim/vk/caching/PrefsImpl.java
+++ VK_Client/app/src/main/java/com/nethergrim/vk/caching/PrefsImpl.java
@@ -3,5 +3,5 @@ package com.nethergrim.vk.caching;
 /**
  * @author andreydrobyazko on 3/20/15.
  */
-public class PrefsImpl {
+public class PrefsImpl implements Prefs {
 }
--- VK_Client/app/src/main/java/com/nethergrim/vk/fragment/MessagesFragment.java
+++ VK_Client/app/src/main/java/com/nethergrim/vk/fragment/MessagesFragment.java
@@ -2,11 +2,15 @@ package com.nethergrim.vk.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nethergrim.vk.R;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
 
 /**
  * @author andreydrobyazko on 3/20/15.
@@ -22,5 +26,32 @@ public class MessagesFragment extends AbstractFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        VKRequest request = new VKRequest("messages.getDialogs");
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                Log.e("TAG", "complete");
+            }
+
+            @Override
+            public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
+                super.attemptFailed(request, attemptNumber, totalAttempts);
+                Log.e("TAG", "attempt failed");
+            }
+
+            @Override
+            public void onError(VKError error) {
+                super.onError(error);
+                Log.e("TAG", "error");
+            }
+
+            @Override
+            public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded, long bytesTotal) {
+                super.onProgress(progressType, bytesLoaded, bytesTotal);
+                Log.e("TAG", "on progress");
+            }
+        });
     }
 }
