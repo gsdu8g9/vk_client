8d00acdf3c15758e98c263f7a5e0fe07bccad86e
nethergrim <c2q9450@gmail.com>
2015-04-18 22:48:40 +0300

    [VK-3] - implemented rouned avatars by picasso

--- VK_Client/app/src/main/java/com/nethergrim/vk/web/ImageLoaderImpl.java
+++ VK_Client/app/src/main/java/com/nethergrim/vk/web/ImageLoaderImpl.java
@@ -1,7 +1,12 @@
 package com.nethergrim.vk.web;
 
 import android.content.Context;
-import android.util.Log;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -19,7 +24,41 @@ public class ImageLoaderImpl implements ImageLoader {
 
     @Override
     public void displayAvatar(String url, ImageView imageView) {
-        Log.e("TAG", "displaying: " + url);
-        Picasso.with(context).load(url).fit().centerCrop().into(imageView);
+        Picasso.with(context).load(url).fit().centerCrop().noFade().config(Bitmap.Config.RGB_565).transform(new RoundedTransformation()).into(imageView);
+    }
+
+    public class RoundedTransformation implements com.squareup.picasso.Transformation {
+
+
+        @Override
+        public Bitmap transform(final Bitmap source) {
+            int radius = Math.max(source.getWidth(), source.getHeight()) / 2;
+
+            final Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+
+            Bitmap output = Bitmap.createBitmap(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(output);
+            canvas.drawCircle((source.getWidth()) / 2, (source.getHeight()) / 2, radius - 2, paint);
+
+            if (source != output) {
+                source.recycle();
+            }
+
+            Paint paint1 = new Paint();
+            paint1.setColor(Color.WHITE);
+            paint1.setStyle(Paint.Style.STROKE);
+            paint1.setAntiAlias(true);
+            canvas.drawCircle((source.getWidth()) / 2, (source.getHeight()) / 2, radius - 2, paint1);
+
+
+            return output;
+        }
+
+        @Override
+        public String key() {
+            return "rounded";
+        }
     }
 }
