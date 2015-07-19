984390fac8588b56d168001ae05091aac38531c4
nethergrim <c2q9450@gmail.com>
2015-07-19 20:03:37 +0300

    [VK-19] - Improved quality of avatar placeholder image, and group chat image. Switched from Glide to Picasso. Removed CirclularImageView from the conversation view.

--- VK_Client/app/src/main/java/com/nethergrim/vk/web/images/PicassoImageLoaderImpl.java
+++ VK_Client/app/src/main/java/com/nethergrim/vk/web/images/PicassoImageLoaderImpl.java
@@ -8,10 +8,12 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Shader;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.nethergrim.vk.R;
 import com.nethergrim.vk.models.User;
+import com.nethergrim.vk.utils.UserUtils;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -26,16 +28,35 @@ public class PicassoImageLoaderImpl implements ImageLoader {
     }
 
     @Override
-    public void displayUserAvatar(@NonNull User user,@NonNull  ImageView imageView) {
+    public void displayUserAvatar(@NonNull User user, @NonNull ImageView imageView) {
         boolean deactivated = user.getDeactivated() != null && user.getDeactivated().length() > 0;
         if (deactivated) {
-            Picasso.with(context).load(R.drawable.ic_deactivated_200).fit().centerCrop().noFade().config(Bitmap.Config.RGB_565).transform(new RoundedTransformation()).into(imageView);
+            Picasso.with(context)
+                    .load(R.drawable.ic_deactivated_200)
+                    .fit()
+                    .centerCrop()
+                    .config(Bitmap.Config.RGB_565)
+                    .transform(new RoundedTransformation())
+                    .into(imageView);
         } else {
-            String url = user.getPhoto_200();
-            if (url != null && url.length() > 0 && url.contains("http")) {
-                Picasso.with(context).load(url).fit().centerCrop().noFade().config(Bitmap.Config.RGB_565).transform(new RoundedTransformation()).into(imageView);
+            String url = UserUtils.getStablePhotoUrl(user);
+            if (!TextUtils.isEmpty(url)) {
+                Picasso.with(context)
+                        .load(url)
+                        .fit()
+                        .centerCrop()
+                        .config(Bitmap.Config.RGB_565)
+                        .placeholder(R.drawable.ic_action_account_circle)
+                        .transform(new RoundedTransformation())
+                        .into(imageView);
             } else {
-                Picasso.with(context).load(R.drawable.ic_action_account_circle).fit().centerCrop().noFade().config(Bitmap.Config.RGB_565).transform(new RoundedTransformation()).into(imageView);
+                Picasso.with(context)
+                        .load(R.drawable.ic_action_account_circle)
+                        .fit()
+                        .centerCrop()
+                        .config(Bitmap.Config.RGB_565)
+                        .transform(new RoundedTransformation())
+                        .into(imageView);
             }
         }
 
@@ -52,7 +73,8 @@ public class PicassoImageLoaderImpl implements ImageLoader {
             paint.setAntiAlias(true);
             paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
 
-            Bitmap output = Bitmap.createBitmap(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
+            Bitmap output = Bitmap.createBitmap(source.getWidth(), source.getHeight(),
+                    Bitmap.Config.ARGB_8888);
             Canvas canvas = new Canvas(output);
             canvas.drawCircle((source.getWidth()) / 2, (source.getHeight()) / 2, radius - 2, paint);
 
@@ -64,7 +86,8 @@ public class PicassoImageLoaderImpl implements ImageLoader {
             paint1.setColor(Color.WHITE);
             paint1.setStyle(Paint.Style.STROKE);
             paint1.setAntiAlias(true);
-            canvas.drawCircle((source.getWidth()) / 2, (source.getHeight()) / 2, radius - 2, paint1);
+            canvas.drawCircle((source.getWidth()) / 2, (source.getHeight()) / 2, radius - 2,
+                    paint1);
             return output;
         }
 
