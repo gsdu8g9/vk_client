07d0e3ef92860b62afc0c64a4c7dd5f6149131da
nethergrim <c2q9450@gmail.com>
2015-07-27 14:48:20 +0300

    [VK-16] - created audion model

--- VK_Client/app/src/main/java/com/nethergrim/vk/models/Audio.java
+++ VK_Client/app/src/main/java/com/nethergrim/vk/models/Audio.java
@@ -1,6 +1,7 @@
 package com.nethergrim.vk.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.realm.RealmObject;
 
@@ -10,25 +11,114 @@ import io.realm.RealmObject;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Audio extends RealmObject {
 
+    @JsonProperty("id")
     private long id;
 
-    private long owner_id;
+    @JsonProperty("owner_id")
+    private long ownerId;
 
-
+    @JsonProperty("artist")
     private String artist;
 
+    @JsonProperty("title")
     private String title;
 
+    @JsonProperty("duration")
     private long duration;
 
+    @JsonProperty("url")
     private String url;
 
-    private long lyrics_id;
+    @JsonProperty("lyrics_id")
+    private long lyricsId;
 
-    private long album_id;
+    @JsonProperty("album_id")
+    private long albumId;
 
-    private long genre_id;
+    @JsonProperty("genre_id")
+    private long genreId;
 
+    @JsonProperty("date")
     private long date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public long getLyricsId() {
+        return lyricsId;
+    }
+
+    public void setLyricsId(long lyricsId) {
+        this.lyricsId = lyricsId;
+    }
+
+    public long getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(long albumId) {
+        this.albumId = albumId;
+    }
+
+    public long getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(long genreId) {
+        this.genreId = genreId;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
 
 }
