b5600adb351722ade83637cc55aafd1286bfaba4
nethergrim <c2q9450@gmail.com>
2015-07-27 14:56:35 +0300

    [VK-16] - finished, displaying audio in conversations list item.

--- VK_Client/app/src/main/java/com/nethergrim/vk/utils/MessageUtils.java
+++ VK_Client/app/src/main/java/com/nethergrim/vk/utils/MessageUtils.java
@@ -2,8 +2,11 @@ package com.nethergrim.vk.utils;
 
 import android.support.annotation.NonNull;
 
+import com.nethergrim.vk.models.Attachment;
 import com.nethergrim.vk.models.Message;
 import com.nethergrim.vk.models.Sticker;
+
+import java.util.List;
 
 /**
  * @author Andrey Drobyazko (c2q9450@gmail.com).
@@ -26,8 +29,31 @@ public class MessageUtils {
     }
 
     public static boolean isMessageWithPhoto(@NonNull Message message) {
-        return isMessageWithAttachments(message)
-                && message.getAttachments().get(0).getPhoto() != null;
+        boolean result = false;
+        if (isMessageWithAttachments(message)) {
+            List<Attachment> attachmentList = message.getAttachments();
+            for (int i = 0, size = attachmentList.size(); i < size; i++) {
+                if (attachmentList.get(i).getPhoto() != null) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean isMessageWithAudio(@NonNull Message message) {
+        boolean result = false;
+        if (isMessageWithAttachments(message)) {
+            List<Attachment> attachmentList = message.getAttachments();
+            for (int i = 0, size = attachmentList.size(); i < size; i++) {
+                if (attachmentList.get(i).getAudio() != null) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
 }
